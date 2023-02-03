e3c8c85556577c8cfbdddd17c928031bf763fb5a
enjoyneer87 <enjoyneer87@naver.com>
2023-02-03 16:05:48 +0900

    commit before move to class @

--- Class/fcn_fft_circuit.m
+++ Class/fcn_fft_circuit.m
@@ -1,9 +1,14 @@
-function X=fcn_fft_circuit(data,waveform)
+function X=fcn_fft_circuit(data,waveform,PN)
     X=fft(waveform);
     N=length(waveform);
+    if PN==1 %Positive=1 all=0
     %Positive Side
     Side = X(1:N/2);
     fft_X = abs(Side)/(N/2);
+    else 
+    Side = X(1:N);
+    fft_X = real(Side)/N;
+    end
     
     if data.p == 12
     ticks=[1 : 1 : 180];
