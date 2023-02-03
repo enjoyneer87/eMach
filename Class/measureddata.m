cc9cc0ec50701a8e7b924b4c2f8043dd5dd71dc1
enjoyneer87 <enjoyneer87@naver.com>
2023-02-03 15:54:23 +0900

    before move @

--- Class/measureddata.m
+++ Class/measureddata.m
@@ -1,20 +1,29 @@
-classdef measureddata < Machinedata
+classdef measureddata < emlab_MachineData
     properties
-    test_rpm
-    wr_plot
-    wr_test
+
     I1=struct('unit','A')
     I2=struct('unit','A')
     I3=struct('unit','A')
     Iabc=table()
     FFT_Iabc=table()
+    FFT_Idq=table()
 %     I3=-I1-I2
     u1=struct()
     u2=struct()  
     u3=struct()
-
+    FFT_uabc=table()
 
 %     u3=-u1-u2
+   
+    fluxlink_1=struct()
+    fluxlink_2=struct()  
+    fluxlink_3=struct()
+    FFT_fluxlinkabc=table()
+    elec_torque
+    shaft_torque
+    end
+    methods
+        phasor_diagram(obj)
     end
 
 end
