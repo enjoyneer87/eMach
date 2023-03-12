5a901ad5c1abd1b268abc9aea2a1164a222288c5
enjoyneer87 <enjoyneer87@naver.com>
2023-03-13 07:18:06 +0900

    add some  주석

--- Class/ResultMotorcadLabData.m
+++ Class/ResultMotorcadLabData.m
@@ -46,29 +46,33 @@ classdef ResultMotorcadLabData < ResultMotorcadData & MotorcadData
             obj.ArmatureConductor_Temperature=motorcadDataObj.ArmatureConductor_Temperature;
             obj.Magnet_Temperature=motorcadDataObj.Magnet_Temperature;
         end
-            function obj = tempCorrectedModelParameters_MotorLAB(obj, tempCoeffiBrAlpha)
-        % 온도 보정 계수 할당
-        obj.tempCoeffiBrAlpha = tempCoeffiBrAlpha;
-%         obj.tempCoeffiHcBeta = tempCoeffiHcBeta;
+                
+        function obj = tempCorrectedModelParameters_MotorLAB(obj, tempCoeffiBrAlpha)
+            % 온도 보정 계수 할당
+            obj.tempCoeffiBrAlpha = tempCoeffiBrAlpha;
+    %         obj.tempCoeffiHcBeta = tempCoeffiHcBeta;
+        
+            % 기존 ModelParameters_MotorLAB에서 변환한 값을 저장할 새로운 변수 생성
+            % 새로운 온도에서 PsiDModel_Lab 및 PsiQModel_Lab 계산
+            obj.postModelParameters_MotorLAB = obj.ModelParameters_MotorLAB;
+
+            %%무부하만  ratio 를 사용하고
+            obj.postModelParameters_MotorLAB.PsiDModel_Lab(1,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(1,:) .* (1 + obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
+            obj.postModelParameters_MotorLAB.PsiQModel_Lab(1,:) = obj.ModelParameters_MotorLAB.PsiQModel_Lab(1,:) .* (1 + obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
+    %         obj.postModelParameters_MotorLAB.PsiQModel_Lab = obj.ModelParameters_MotorLAB.PsiQModel_Lab.* (1 - obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
     
-        % 기존 ModelParameters_MotorLAB에서 변환한 값을 저장할 새로운 변수 생성
-        % 새로운 온도에서 PsiDModel_Lab 및 PsiQModel_Lab 계산
-        obj.postModelParameters_MotorLAB = obj.ModelParameters_MotorLAB;
-        obj.postModelParameters_MotorLAB.PsiDModel_Lab(1,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(1,:) .* (1 + obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
-        obj.postModelParameters_MotorLAB.PsiQModel_Lab(1,:) = obj.ModelParameters_MotorLAB.PsiQModel_Lab(1,:) .* (1 + obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
-%         obj.postModelParameters_MotorLAB.PsiQModel_Lab = obj.ModelParameters_MotorLAB.PsiQModel_Lab.* (1 - obj.tempCoeffiBrAlpha/100 .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
-
-        %         obj.postModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) .* (1 + obj.psiDTempRatio(2:end,:) .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
-%         obj.postModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)  = obj.ModelParameters_MotorLAB.PsiQModel_Lab(2:end,:) .* (1 + obj.psiQTempRatio(2:end,:) .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
-        obj.postModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(2:end,:)*1 + obj.psiDTempRatio(2:end,:)/100* (obj.postMagnet_Temperature - obj.Magnet_Temperature);
-        obj.postModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)  = obj.ModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)*1 %+ obj.psiQTempRatio(2:end,:)/100* (obj.postMagnet_Temperature - obj.Magnet_Temperature);
-         
-        %         obj.postModelParameters_MotorLAB.PsiDModel_Lab=obj.ModelParameters_MotorLAB.PsiDModel_Lab.*obj.psiDTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature);
-%         obj.postModelParameters_MotorLAB.PsiQModel_Lab=obj.ModelParameters_MotorLAB.PsiQModel_Lab.*obj.psiQTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature);
-%         refTempMotorcadPost.psiQTempRatio=(refTempMotorcad.ModelParameters_MotorLAB.PsiQModel_Lab/refTempMotorcad.ModelParameters_MotorLAB.PsiQModel_Lab-1)/(Temp1-Tempref)
-%         obj.postModelParameters_MotorLAB.PsiDModel_Lab=(obj.psiDTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature)+1).*obj.ModelParameters_MotorLAB.PsiDModel_Lab;
-%         obj.postModelParameters_MotorLAB.PsiQModel_Lab=(obj.psiQTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature)+1).*obj.ModelParameters_MotorLAB.PsiQModel_Lab;
-
-            end
+            %% 부하상태는 두개의 해석으로부터 scailing 
+            %         obj.postModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) .* (1 + obj.psiDTempRatio(2:end,:) .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
+    %         obj.postModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)  = obj.ModelParameters_MotorLAB.PsiQModel_Lab(2:end,:) .* (1 + obj.psiQTempRatio(2:end,:) .* (obj.postMagnet_Temperature - obj.Magnet_Temperature));
+            obj.postModelParameters_MotorLAB.PsiDModel_Lab(2:end,:) = obj.ModelParameters_MotorLAB.PsiDModel_Lab(2:end,:)*1 + obj.psiDTempRatio(2:end,:)/100* (obj.postMagnet_Temperature - obj.Magnet_Temperature);
+            obj.postModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)  = obj.ModelParameters_MotorLAB.PsiQModel_Lab(2:end,:)*1 %+ obj.psiQTempRatio(2:end,:)/100* (obj.postMagnet_Temperature - obj.Magnet_Temperature);
+             
+            %         obj.postModelParameters_MotorLAB.PsiDModel_Lab=obj.ModelParameters_MotorLAB.PsiDModel_Lab.*obj.psiDTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature);
+    %         obj.postModelParameters_MotorLAB.PsiQModel_Lab=obj.ModelParameters_MotorLAB.PsiQModel_Lab.*obj.psiQTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature);
+    %         refTempMotorcadPost.psiQTempRatio=(refTempMotorcad.ModelParameters_MotorLAB.PsiQModel_Lab/refTempMotorcad.ModelParameters_MotorLAB.PsiQModel_Lab-1)/(Temp1-Tempref)
+    %         obj.postModelParameters_MotorLAB.PsiDModel_Lab=(obj.psiDTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature)+1).*obj.ModelParameters_MotorLAB.PsiDModel_Lab;
+    %         obj.postModelParameters_MotorLAB.PsiQModel_Lab=(obj.psiQTempRatio*(obj.postMagnet_Temperature-obj.Magnet_Temperature)+1).*obj.ModelParameters_MotorLAB.PsiQModel_Lab;
+    
+        end
     end
 end
