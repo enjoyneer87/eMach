0ce58a753687f612cf2c84b49eca740309cc8aaa
kangsg9790 <kangsg9790@gmail.com>
2023-08-09 11:46:22 +0900

    함수 출력 struct 구조 정리,  각 함수 기본 셋팅 내용 포함

--- SkkuEMLabProject/MotorCAD_Lab_BasePoint.m
+++ SkkuEMLabProject/MotorCAD_Lab_BasePoint.m
@@ -1,4 +1,4 @@
-function [Power, Torque, Speed] = MotorCAD_Lab_BasePoint(mcad, mcad_file_dir, mcad_file_name)
+function BasePointOutput = MotorCAD_Lab_BasePoint(mcad, mcad_file_dir, mcad_file_name, input)
 
 % 임시 
 % mcad=actxserver('motorcad.appautomation');
@@ -7,7 +7,52 @@ function [Power, Torque, Speed] = MotorCAD_Lab_BasePoint(mcad, mcad_file_dir, mc
 % data=load('E:\2023_Task\etc\14_EMLAB_TractionMotor\04_CODE\MCADSpary\Model1e2Spray2023\Lab\MotorLAB_elecdata_copy.mat');
 
 %% 셋팅
+% Calculation - General
+input.DCBusVoltage                               = 800;      % Drive - DC Bus Voltage
+input.ModulationIndex_MotorLAB                   = 0.95;     % Drive - Maximum Modulation Index
+input.OperatingMode_Lab                          = 0;        % Drive - Operating Mode : Motor
+input.ControlStrat_MotorLAB                      = 0;        % Drive - Control Stratey : Maximum Torque/Amp
+input.DCCurrentLimit_Lab                         = 0;        % Drive - DC Current Limit : None
+
+input.MagnetLossBuildFactor                      = 1 ;       % Losses - Magnet Loss Build Factor
+input.StatorIronLossBuildFactor                  = 1 ;       % Losses - Iron Loss Build Factors - Stator
+input.RotorIronLossBuildFactor                   = 1 ;       % Losses - Iron Loss Build Factors - Rotor
+
+% input.TurnsRef_MotorLAB                          = [];       % Scaling
+% input.ResistanceTurnsRef_MotorLAB                = [];       % Scaling
+input.TurnsCalc_MotorLAB                         = 1;        % Scaling - Turns / Coil - Calculation
+
+% input.Length_Ref_Lab                             = [];       % Scaling
+% input.Length_Ref_Resistance_Lab                  = [];       % Scaling
+% input.Length_Calc_Lab                            = [];       % Scaling - Active Length - Calculation
+
+input.TwindingCalc_MotorLAB                      = 65;       % Temperatures - Stator Winding Temperature - Calculation temperature
+input.StatorCopperFreqCompTempExponent           = 1;        % Temperatures - Stator Winding Temperature - AC Loss Temperature Scailing - Temperature Exponent
+input.TmagnetCalc_MotorLAB                       = 65;       % Temperatures - Magnet Temperature - Calculation temperature
+
+% Elecromagnetic
+input.EmagneticCalcType_Lab                     = 1;        % Calculation - Calculation Type : Efficiency Map
+input.SmoothMap_MotorLAB                        = true;     % Calculation - Options - Smooth Map : "On"
+input.PowerLim_MotorLAB                         = false;    % Calculation - Options - Power Limit : "Off"
+if input.PowerLim_MotorLAB==true || input.PowerLim_MotorLAB==1
+    input.PowerLimVal_MotorLAB                  = 1000000;  % Calculation - Options - Power Limit - Max Power
+end
+input.SpeedMax_MotorLAB                         = 25000;    % Calculation - Speed - Maximum
+input.Speedinc_MotorLAB                         = 200;      % Calculation - Speed - Step Size
+input.SpeedMin_MotorLAB                         = 200;      % Calculation - Speed - Minimum
+input.Imax_MotorLAB                             = [];       %
+input.Imax_RMS_MotorLAB                         = 500;      % Calculation - Current - Maximum (RMS)
+input.Iinc_MotorLAB                             = 10;       % Calculation - Current - No. of Increments
+input.Imin_MotorLAB                             = [];       %
+input.Imin_RMS_MotorLAB                         = 0;       % Calculation - Current - Minimum (RMS)
+input.TorqueMax_MotorLAB                        = [];       %
+input.TorqueInc_MotorLAB                        = [];       %
+input.MinTorque_MotorLAB                        = [];       %
+
+%% 해석
 % 전자계 해석 실행 & 결과 자동 저장 'MotorLAB_elecdata'
+setMcadVariable(input,mcad);
+mcad.SetVariable("MessageDisplayState", 2);
 mcad.CalculateMagnetic_Lab();
 
 % 파일 복사
@@ -37,9 +82,7 @@ TorqueSlope=(data.Shaft_Torque(1,NumberOfIncrements)-data.Shaft_Torque(BaseSpeed
 BaseTorque_modified=data.Shaft_Torque(1,NumberOfIncrements)-BaseSpeed_modified*TorqueSlope;
 MaximumPower_modified=BaseTorque_modified*BaseSpeed_modified/60*2*pi;
 
-Power=MaximumPower_modified;
-Torque=BaseTorque_modified;
-Speed=BaseSpeed_modified;
+BasePointOutput=table2struct(table(MaximumPower_modified, BaseTorque_modified, BaseSpeed_modified));
 
 end
 
