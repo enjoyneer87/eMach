6bb63d38b309749ae3f19eaf97a315bd0167ddf5
enjoyneer87 <enjoyneer87@naver.com>
2023-08-17 07:17:42 +0900

    creaDoECheckTable

--- SkkuEMLabProject/createDoECheckTable.m
+++ SkkuEMLabProject/createDoECheckTable.m
@@ -1,5 +1,7 @@
 function newDesignFolderTable = createDoECheckTable(refMotFilePath, DoETable, Type)
     % 디자인 폴더 경로 테이블 생성
+    MotFileList                       =findMOTFiles(fileparts(refMotFilePath));
+    removeAutoSaveFiles(MotFileList)
     if nargin == 3
         newDesignFolderTable = createNewDesignFolderTable(refMotFilePath, DoETable, Type);
     elseif nargin == 2
--- SkkuEMLabProject/createNewDesignFolderTable.m
+++ SkkuEMLabProject/createNewDesignFolderTable.m
@@ -6,7 +6,7 @@ function newDesignFolderTable = createNewDesignFolderTable(refMotFilePath, DoETa
     newDesignFolderTable = table('Size', [numFolders, 1], 'VariableTypes', {'string',}, 'VariableNames', {'DesignFolderPath'});
 
     % Type 값에 따라 다른 동작 수행
-    if nargin < 3 || (ischar(Type) && strcmpi(Type, 'Matlab')) || (isnumeric(Type) && Type == 0)
+    if nargin < 3 || (ischar(Type) && strcmpi(Type, 'matlab')) || (isnumeric(Type) && Type == 0)
         % 각 케이스에 대한 새 폴더 경로 생성 및 테이블에 추가
         for CaseNumber = 1:numFolders
             newFolder = createNewFolder(refMotFilePath, CaseNumber);    % 새 폴더 경로 생성
