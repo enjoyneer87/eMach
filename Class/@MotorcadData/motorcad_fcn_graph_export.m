9e4a77fab7ecf395f3e727ff72f95f4432f747b3
enjoyneer87 <enjoyneer87@naver.com>
2023-09-04 17:35:41 +0900

    [NF]Mesh& DXF Plot Flux Locus Plot (data from Jmag)

--- Class/@MotorcadData/motorcad_fcn_graph_export.m
+++ Class/@MotorcadData/motorcad_fcn_graph_export.m
@@ -1,10 +1,10 @@
 function res=jmag_fcn_graph_export(o_data_name)
-global jmag_version
-global ModelName
-global StudyName
-jv=jmag_version
-jmag = actxserver(strcat('designer.Application.',jv));
-res=strcat(pwd,'/',o_data_name,'.csv')
-jmag.GetModel(ModelName).GetStudy(StudyName).GetResultTable().GetData(o_data_name).WriteTable(res,"Time")
+    global jmag_version
+    global ModelName
+    global StudyName
+    jv=jmag_version
+    jmag = actxserver(strcat('designer.Application.',jv));
+    res=strcat(pwd,'/',o_data_name,'.csv')
+    jmag.GetModel(ModelName).GetStudy(StudyName).GetResultTable().GetData(o_data_name).WriteTable(res,"Time")
 
 end 
