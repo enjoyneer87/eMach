2f95430b079dfedb5d0bbda339d213bbd20a2196
enjoyneer87 <enjoyneer87@naver.com>
2023-12-15 14:34:18 +0900

    [WIP]before split devJmagSettingV2 branch

--- tools/jmag/ImportDXF2Geom.m
+++ tools/jmag/ImportDXF2Geom.m
@@ -6,12 +6,17 @@ function sketchs=ImportDXF2Geom(dxfList2Import,app)
     geomAssem=geomDocu.GetAssembly();
 
     sketchs=struct();
+    if length(dxfList2Import)>1
     ItemList={"Stator","Rotor"};
 
  %%
     % Define Sketch Names
+    
     sketchs(1).sketchName=ItemList{1};
     sketchs(2).sketchName=ItemList{2};
+    else
+    sketchs(1).sketchName='Stator';
+    end
 
     for i=1:length(dxfList2Import)  
         geomApp.Merge(dxfList2Import(i).dxfPath);
